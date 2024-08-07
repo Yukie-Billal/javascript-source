1f04945685dbcd16df1651760cb8230b040f6824
Yukie M Billal <yukiembillal01@gmail.com>
2024-08-07 14:53:29 +0700

    feat: youtube solve problem

--- EventDelegation/script.js
+++ EventDelegation/script.js
@@ -2,21 +2,18 @@ const buttonAddBox = document.querySelector('.event-container .event-button')
 const boxes = document.querySelectorAll('.container .box')
 const boxContainer = document.querySelector('.container')
 
-const boxClickEventHandler = el => {
-   el.classList.toggle("event")
+addGlobalEventListener = (event, selector, callback, parent = document) => {
+   parent.addEventListener(event, e => {
+      if (e.target.matches(selector)) {
+         callback(e)
+      }
+   })
 }
 
-const buttonAddEventHandler = () => {
+addGlobalEventListener("click", ".event-button", () => {
    const box = document.createElement('div')
    box.classList.add('box')
    boxContainer.append(box)
-}
-
-document.addEventListener("click", e => {
-   console.log(e.target);
-   if (e.target.classList.contains('box')) {
-      boxClickEventHandler(e.target)
-   } else if (e.target.classList.contains('event-button')) {
-      buttonAddEventHandler(e.target)
-   }
 })
+
+addGlobalEventListener("click", ".box", e => e.target.classList.toggle("event"), boxContainer)
